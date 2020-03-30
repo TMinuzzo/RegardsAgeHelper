ef767f7fcc58f3b73ad66120843f940048be2972
thayna.minuzo@hp.com <thaynaminuzzo@gmail.com>
2020-03-30 16:26:09 -0300

    Added Potions and Plants

--- dictionary.js
+++ dictionary.js
@@ -1,10 +1,93 @@
 const Glossary = require("./glossary.js");
 
 function matchMeaning(args) {
-  let arg = args[0].toLowerCase();
+  let arg = args[0].toUpperCase();
   switch (arg) {
-    case "mana":
-      return Glossary.Mana;
+    case "MANA":
+      return Glossary.General.Mana;
+    /* General */
+    case "MAGO":
+    case "MAGOS":
+    case "FEITICEIRO":
+    case "FEITICEIROS":
+      return Glossary.General.MageSorcerer;
+
+    /* Plants */
+    case "FLOUREN":
+      return Glossary.Plants.Flouren;
+    case "PERILACEA":
+      return Glossary.Plants.Perilacea;
+    case "HERIM":
+      return Glossary.Plants.Herim;
+    case "CORUS":
+      return Glossary.Plants.Corus;
+    case "SURINAN":
+      return Glossary.Plants.Surinan;
+    case "TRIGALIA":
+      return Glossary.Plants.Trigalia;
+    case "ORIÇUA":
+    case "ORICUA":
+      return Glossary.Plants.Oriçua;
+    case "XASCENNE":
+      return Glossary.Plants.Xascenne;
+    case "FLAMINGA":
+      return Glossary.Plants.Flaminga;
+    case "MANDES":
+      return Glossary.Plants.Mandes;
+    case "ROBARA":
+      return Glossary.Plants.Robara;
+    case "LAZIR":
+      return Glossary.Plants.Lazir;
+    case "TALANUA":
+      return Glossary.Plants.Talanua;
+    case "KARNES":
+      return Glossary.Plants.Karnes;
+    case "EMILVAL":
+      return Glossary.Plants.Emilval;
+    case "MALANGUA":
+      return Glossary.Plants.Malagua;
+    case "ZORLAU":
+      return Glossary.Plants.Zorlau;
+    case "OPHIOCARDO":
+      return Glossary.Plants.Ophiocardo;
+    case "BABUIM":
+      return Glossary.Plants.Babuim;
+    case "HERAQUITA":
+      return Glossary.Plants.Heraquita;
+    case "CALICGO":
+      return Glossary.Plants.Calicgo;
+
+    /* Alchemy Potions */
+    case "TRUTUM":
+      return Glossary.Potions.Trutum;
+    case "FALANTIS BEINGS":
+      return Glossary.Potions.FalantisBeings;
+    case "REVERTEK":
+      return Glossary.Potions.Revertek;
+    case "RADARIS":
+      return Glossary.Potions.Radaris;
+    case "GETRIQUIS":
+      return Glossary.Potions.Getriquis;
+    case "LYTOGROW":
+      return Glossary.Potions.Lytogrow;
+    case "TRANSMOG":
+      return Glossary.Potions.Transmog;
+    case "BURBLAN":
+      return Glossary.Potions.Burblam;
+    case "PYROTIC":
+      return Glossary.Potions.Pyrotic;
+    case "SULFRIC":
+      return Glossary.Potions.Sulfric;
+    case "UNMORTALIS":
+      return Glossary.Potions.Unmortalis;
+    case "OILIS":
+      return Glossary.Potions.Oilis;
+    case "HEALING POTION":
+      return Glossary.Potions.HealingPotion;
+    case "HUMANTRI":
+      return Glossary.Potions.Humantri;
+    default:
+      return "Nome não reconhecido.";
   }
 }
 
--- glossary.js
+++ glossary.js
@@ -1,6 +1,83 @@
 const Glossary = {
-  Mana:
-    "A Mana é um recurso natural, infinito, invisível como o ar e intocável como a luz, que pode ser sentido, absorvido e até manipulado internamente por alguns seres vivos aleatórios que já nascem com esta habilidade, mas muitos nunca descobrem em vida que eram capazes de tal. A absorção é lenta (3 de mana por rodada para seres vivos comuns) e cada indivíduo tem sua capacidade máxima de armazenamento no corpo (10 de mana para seres vivos comuns). Feitiços arcanos, que são mana concentrada e controlada, têm uma coloração Azul aos olhos humanos."
+  General: {
+    Mana:
+      "A Mana é um recurso natural, infinito, invisível como o ar e intocável como a luz, que pode ser sentido, absorvido e até manipulado internamente por alguns seres vivos aleatórios que já nascem com esta habilidade, mas muitos nunca descobrem em vida que eram capazes de tal. A absorção é lenta (3 de mana por rodada para seres vivos comuns) e cada indivíduo tem sua capacidade máxima de armazenamento no corpo (10 de mana para seres vivos comuns). Feitiços arcanos, que são mana concentrada e controlada, têm uma coloração Azul aos olhos humanos.",
+    MageSorcerer:
+      "É considerado um “Feiticeiro” aquele que nasceu com o dom de utilizar feitiços arcanos, independente de possuir alguma habilidade com o domínio das gemas. “Mago”, entretanto é aquele que consegue lançar magias através de seu controle das gemas elementares, e pode ou não ser um feiticeiro ao mesmo tempo."
+  },
+  Plants: {
+    Flouren:
+      " Cogumelo verde fluorescente que cresce em locais escuros e profundos, não é comestível e costuma atrair insetos para sua luz. Originalmente Flourens eram raros e desconhecidos por todo o mundo, até que os underall o encontraram em uma caverna subterrânea muito profunda junto de formigas de lava. Após replantarem muitas vezes estes cogumelos, depois de séculos ele se tornou conhecido no mundo todo e uma ferramenta de iluminação padrão nas cidades subterrâneas do território underall. ",
+    Perilacea:
+      "Uma erva parecida com capim, muito comprida, se divide em 3 na ponta e  cresce em matagais, fácil de encontrar em maior parte do mundo. Chá de Perilácea é popularmente usado na medicina para ajudar a digerir e limpar o estômago, mas se a dosagem for muito grande causa fortes diarreias. Pode ser misturada com pó de osso e qualquer tipo de liquido ácido para criar um veneno que destrói o estômago de uma pessoa, causando hemorragia interna e matando em até uma hora.",
+    Herim:
+      "Grande flor branca abençoada pela luz, usada popularmente como decoração de templos da luz e como um pequeno amuleto para afastar demônios (só funciona realmente contra demônios pequenos). Celestiais da raça Sprite vivem dentro dessas flores em locais afastados da civilização.",
+    Corus:
+      "Uma família especial de cogumelos que crescem em regiões frias, sua estipe é feita de alguma planta leguminosa como cenoura ou batata (Córus-cenoura, Córus-batata), tendo apenas um pequeno pedaço do legume visível fora da terra. Além de se alimentar de nutrientes da terra, os Córus absorvem restos de animais como pele, carne e sangue, sendo este o principal adubo para fazê-los crescer. É um fungo popular entre os raven, sendo considerado benção sagrada e é parte de rituais religiosos, onde no fim de cada dia os raven jogam restos de animais em suas plantações de Córus para abençoa-los com muitos legumes. Córus é um dos principais alimentos entre os raven.",
+    Surinan:
+      "Uma árvore considerada como praga em todo o mundo, mesmo entre druidas quando esta árvore é avistada é recomendável que seja cortada imediatamente. Suas raízes são finas compridas e se alargam muito, roubando nutrientes de outras plantas por perto. Seu tronco é fino e cresce muito rápido, se apoiando em outras árvores próximas, quase como uma trepadeira que não se enrola, e a altura máxima da Surinan pode ser tão grande quando um eucalipto e suas folhas são largas como uma palmeira, criando áreas com muita sombra, impedindo que outras plantas mais baixas recebam luz. Sua proliferação se da por Anemocoria. É encontrada principalmente em Northlon e Gamênia, mas não é comum pois são desvastadas assim que são encontradas, se não todas as plantas na região morrem.",
+    Trigalia:
+      "Árvore de tronco laranja, com madeira muito mais maleável que o comum. Seus vários galhos são finos e compridos, assim como o tronco, e quando venta a Trigália chacoalha por muito tempo. É repleta de acúleos tanto no tronco quanto nos galhos. Popularmente é dito que as Trigálias são arvores dançantes que não gostam de ser interrompidas, chegar perto delas enquanto dançam causa muitos cortes superficiais. A madeira delas é comumente usada para fazer navios, arcos e flechas, por conta de sua maleabilidade.",
+    Oriçua:
+      "Mais popularmente conhecida como 'Pitanga Mortífera', é uma frutinha rosa muito rara, só encontrada em ilhas a nordeste de Sanichini. Tanto animais burros quanto criaturas inteligentes rapidamente aprendem que esta planta é muito perigosa: se comer apenas algumas Oriçuas terá enjoos por algumas horas, sintoma inicial da fruta, e depois de muitas horas irá vomitar sangue rosado, que é o momento em que as dores começam e o destino da pessoa já está selado, e pouco depois morre. A semente de Oriçua na verdade é comestível sem problemas, o veneno está na pequena quantidade de líquido dentro da fruta.",
+    Xascenne:
+      "Uma alga marinha muito grossa e feita quase toda de água, encontrada nas profundezas do oceano, mercadoria que telnos costumam comercializar. Quando seca, a alga fica muito fina e tem a única propriedade de absorver líquidos a sua volta, muito mais rápido que uma esponja, e inclusive absorve a umidade no ar, deixando o ambiente seco depois de horas. A cor da Xascenne também pode ser usada para indicar se a água tem algum tipo de contaminação.",
+    Flaminga:
+      "Flor vermelha que diz-se ser abençoada pelo fogo, é encontrada apenas na terra de regiões vulcânicas e mesmo nelas é rara. A flor é completamente imune a chamas, quando alguém ateia fogo em uma Flaminga, em questão de segundos o fogo se apaga. É uma exótica planta de alto valor.",
+    Mandes:
+      "Família de flores que se parecem com repolhos, com um núcleo grande envolto por varias camadas de pétalas marrom. As pétalas de Mande são carregadas de gases que são liberados caso algo colida com elas, usado pela planta como autodefesa de predadores. O núcleo do Mande é comestível, e bem saboroso, mas é necessária uma técnica muito específica para conseguir cozinha-lo de maneira que não seja tóxico, o clã orc Bothra é conhecido por ter muitos cozinheiros que conhecem bem esta técnica. Há duas variações de Mande: o Mandalul, que possuí um gás alucinógeno mas que pode causar danos permanentes ao pulmões de quem o inspira, embora exista uma droga chamada 'Paraíso' que é feita a partir desta flor de maneira que fique ainda mais alucinógena e menos tóxica; e o Mandel, cujo gás tem uma toxina que inflama temporariamente a garganta da pessoa por algum tempo, causando dificuldades respiratórias e depois de algumas horas cria bolhas de pus dentro do estômago, matando a pessoa internamente com sua acidez. As bolhas de pus podem ser evitadas com muita ingestão de água.",
+    Robara:
+      "Com a casca mais dura e o interior mais mole do mundo, esta árvore é muito única pois serras e machados normais tem muita dificuldade de cortá-la, levando um dia inteiro ou até mais para cortar uma única delas. A unica coisa que consegue cortar com um pouco mais de eficiência sua casta são as serras automáticas dos underall, tornando esta madeira e casca um produto quase exclusivo deles. O interior da Robara branco e mole como um aipim, podendo ser comido depois de cozido, mas não é muito saboroso, os underall são os únicos que apreciam esta comida exótica. A casca da Robara pode ser usada como revestimento para tornar algo mais resistente, e é comum que algumas tribos druidas utilizem um feitiço secreto que enfraquece as plantas para conseguir extrair essa casca e revestir suas roupas de couro para fazer armaduras.",
+    Lazir:
+      "Flor azul que fica aberta para absorver sons altos ao seu redor e então guardando-os internamente. Quando alguém encosta na Lazir fechada ela se abre e solta o som guardado, geralmente espantando animais e até assustando pessoas. Diz-se que esta planta tem propriedades mágicas que a permite guardar os sons, possivelmente abençoada pelo deus dos ventos.",
+    Talanua:
+      "Planta trepadeira comum em Northlon que cria pequenos arbustos ao longo de suas raízes e neles crescem Talans, frutas azuis suculentas que podem ser comidas, mas não são gostosas nem nutritivas, porém podem também ser usadas para fazer tinta azul.",
+    Karnes:
+      "Um tipo de aguapé encontrado em lagos por toda gamênia, possuí um botão branco pequeno no seu topo. Karnés são atraídos por magia arcana e irão flutuar lentamente na direção de onde um feitiço foi lançado ou de uma fonte de magia. É usado em laboratórios experimentais usando sua velocidade de atração para quantificar a quantidade de energia arcana produzida por um feitiço ou objeto mágico.",
+    Emilval:
+      "Árvore negra tanto por fora quanto por dentro, com folhas sempre de cores outonais amarelas ou vermelhas. Sua seiva é branca e muito ácida, usada bastante como reagente químico. Beber sua seiva é extremamente danoso a qualquer criatura. Encontrada principalmente no território dos Raven ou no extremo norte de Northlon.",
+    Malagua:
+      "Mais conhecida como 'Pimenta de Dragão' tem o formato de uma pequena cenoura vermelha e pode ser encontrada em toda Gamênia, principalmente na região central. Sua pimenta é muito forte e a maioria das raças não conseguem tolerar comê-la, mas os lizadios tem resistência maior a este efeito e usam-na frequentemente como tempero. Ao morder Malaguá, uma pequena quantidade de gás inflamável e ardente é liberado, que quando expirado pode causar uma pequena e rápida baforada de fogo caso a pessoa assopre em frente a uma fonte de fogo. ",
+    Zorlau:
+      "Algas marinhas azuis com muitos pequenos cristais que armazenam uma pequenina quantidade de magia arcana, criando um brilho natural. Eram plantas somente encontradas nas grandes profundezas do alto mar, mas há muito tempo os telnos trouxeram esse espécime para Telnia e reproduziram-no em suas cidades, tornando-se uma 'praga' tão forte que se espalhou também para as redondezas de Telnia, e hoje é parte integral de sua civilização, sendo uma luz natural encontrada por toda parte. A quantidade de magia arcana armazenada por cada alga é muito ínfima para que seja relevante de alguma maneira. Durante alguns dias, uma vez por ano, acontece o evento chamado de 'O Encantar das Praias' pelos humanos ou 'O Renascer da Luz' pelos telnos, onde todas Zorlau desprendem seus cristais e começam a crescer novos, os cristais então flutuam até a superfície da água e são levados pela maré até as praias de Northlon onde os oceanos brilham em uma forte luz azul e quando chegam nas areias os cristais se quebram e liberam sua energia de volta para a natureza. Durante este evento anual, as praias ficam carregadas de uma poderosa aura de energia arcana, potencializando enormemente qualquer feitiço lançado no local.",
+    Ophiocardo:
+      "Um fungo que cresce a partir de cadáveres de pessoas infectadas e espalha seus esporos para qualquer um que chegue perto, infectando-os. A única maneira de tratar a infecção do parasita é aquecendo o corpo da pessoa a temperaturas perigosamente altas. Se não for tratada rápido, a pessoa pode sofrer danos permanente ao cérebro. Se nunca for tratada, o parasita ataca o cérebro com 'raízes', deixando-a zumbificada e sem autocontrole, eventualmente a pessoa morre e em seu cadáver cresce em 1 dia um novos talos com esporos para afetar novas pessoas. É encontrado somente nas ilhas e costa a noroeste de Gamênia, os orcs que vivem na região tem pleno conhecimento que o cadáver de alguém infectado deve ser queimado assim que a pessoa morrer.",
+    Babuim:
+      "Planta rasteira similar a uma Babosa que cresce em regiões montanhosas, especialmente ao norte de Gamênia, que possui folhas grossas parecidas com tentáculos verdes. Seu interior contém um líquido pastoso chamado de 'Cera de Babuim' que pode ser usado medicinalmente para tratar ferimentos e pode ser usado para fazer velas. Entretanto, a sua cera é levemente alucinógena, então uma vela funciona como incenso contagiante. É utilizada culturalmente entre os griffin para fazer rituais ou como simples iluminação.",
+    Heraquita:
+      "Conhecida por todas as raças como 'Dente de Songster' (exceto pelos songster, que não gostam desta apologia) é um dente-de-leão com plumas coloridas, como um arco-íris, mas que pode causar forte reação alérgica em algumas pessoas. Encontrado em ilhas e praias de regiões tropicais.",
+    Calicgo:
+      "Um trigo especial, da cor ciano, que fermenta muito mais que trigo comum, o que o torna um reagente químico interessante mas também um ingrediente diferente para se usar na produção de massas e pães, pois estes terão um teor alcoólico razoavelmente alto, podendo até embebedar uma pessoa que comer demais. Seu plantio é comum em largos campos de clima temperado."
+  },
+  Potions: {
+    Trutum: "1 truta + sangue de coelho + medula de hiena. = transforma quem beber em uma truta por 12 horas.",
+    FalantisBeings:
+      "cabeça de papagaio + cogumelos alucinógenos colocados em água fervendo. Mecha bastante e coloque uma pitada de pelo de macaco. = Transforma permanentemente a traquéia de um animal, possibilitando que ele fale.",
+    Revertek:
+      "Coração esmagado de felino + raiz esmagada de uma arvore com mais de 5 metros de altura. Coloque a mistura em um frasco com sangue de um humano com 8 ou menos de inteligência. = Reverte o efeito de outras reações químicas. (felinos grandes rendem 3 frascos)",
+    Radaris:
+      "molhe a orelha de um morcego com sangue de aranha e coloque em um frasco com qualquer bebida corrompida por trevas. = Fica cego por 15 minutos, mas consegue sentir a presença de qualquer coisa que esteja a menos de 20 metros de distância. (aranhas e morcegos gigantes fazem 3 frascos )",
+    Getriquis:
+      "triture 1 dente grande de tigre ártico e coloque em uma mistura de saliva de formiga de formiga ártica gigante com seiva (de qualquer árvore) = A pasta cinza “crocante” resultante por ser derramada e grudada em um objeto para congelar uma pequena camada da superfície de água que entre em contato com o objeto.",
+    Lytogrow:
+      " coloque uma folha de árvore qualquer que tenha sido encantada pela luz/trevas dentro de caldeirão de sangue de dragão (cada dragão rende 1 caldeirão). A reação comprime e condensa o sangue em uma quantidade que cabe em um frasco, também tornando o líquido verde brilhante. = derramar o frasco em terra faz crescer instantaneamente uma árvore falante (do mesmo tipo da folha usada) que defende a luz/trevas com sua vida.",
+    Transmog:
+      "um pedaço de pele de doppleganger + um tufão de pelo de alguma criatura+ uma bebida alcoólica bem forte = Transforma-se na criatura que o pelo foi retirado durante 1 hora.",
+    Burblam:
+      "1 asa de serpente voadora picada em pedaçinhos + água de coco misturados em água do mar = quando fervido, o frasco libera bolhas gigantes (de 1-2 metros de diâmetro) que só estouram quando são golpeadas por algo pontiagudo.",
+    Pyrotic:
+      " 2 linguas de hellhound + 4 penas de fênix + 1 gema de fogo = transforma novamente a gema para ter outra aparência. O novo formato da gema torna o usuário e seus pertences imunes a fogo, além disso o objeto fica passivamente em chamas.",
+    Sulfric:
+      " ataduras de múmias + saliva de worm + limão/maracujá = um ácido que derrete completamente qualquer superfície. (causa 1D12*10 de dano)(1 frasco para cada worm)",
+    Unmortalis:
+      "1 cérebro de zumbi+ pó de fada + sangue de baratas = uma poção que faz o usuário ficar morto por X horas (1 hora para cada barata usada) e depois acorda como se nada tivesse acontecido.",
+    Oilis:
+      "banha de ogro + tinta de kraken + suco de qualquer fruta = um óleo super escorregadio que é basicamente impossível ficar em pé em cima. (1 ogro e 1 fruta rendem 1 frasco, enquanto tinta de um kraken rende para 10)(cada frasco cobre uma área do chão de 2m X 2m)",
+    HealingPotion:
+      "10 folhas de ervas medicinais esmagadas e misturadas em 250ml de sangue humano com um coração inteiro de javali. = Aumenta fortemente a capacidade regenerativa de uma pessoa por uma hora.",
+    Humantri:
+      "1 Osso triturado de esqueleto que tenha sido reanimado por necromancia + água salgada + 4 chifres de devilish colocados de molho por 2 horas. = Um líquido relaxante que quando derramado em cima de um objeto da a ele a habilidade de falar, pensar, e lembrar de tudo que aconteceu em sua “vida”. [dura infinitamente]"
+  }
 };
 
 module.exports = Glossary;
